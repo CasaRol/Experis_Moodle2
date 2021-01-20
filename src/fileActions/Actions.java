01843063ff73fe87fbb1f844592184d44da6a3c3
CasaRol <32219634+CasaRol@users.noreply.github.com>
2021-01-20 15:54:38 +0100

    Init commit via bash

---
+++ src/fileActions/Actions.java
@@ -0,0 +1,58 @@
+package fileActions;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Actions
+ */
+public class Actions {
+
+    public String[] directoryfiles() {
+
+        String[] result;
+
+        File directoryContent = new File("src/assets");
+
+        result = directoryContent.list();
+
+        System.out.println(result);
+
+        return result;
+    }
+
+    public double fileSize(String fileName) {
+
+        final String FILE_PATH = ("src/assets/" + fileName);
+
+        File myFile = new File(FILE_PATH);
+
+        if (!myFile.exists()) {
+            System.out.println("File not found, please try again");
+            System.out.println("Make sure to trype the name correctly aswell as include the file extension");
+            Scanner scan = new Scanner(System.in);
+            String newFile = scan.nextLine();
+            scan.close();
+            fileSize(newFile);
+        }
+        return (myFile.length() / 1024);
+    }
+
+    public int linereader(String fileName) {
+
+        int lines = 0;
+        try(BufferedReader bufRead = new BufferedReader(new FileReader("src/assets/" + fileName))) {
+            while(bufRead.readLine() != null) {
+                lines++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+}
---
+++ src/program.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+import fileActions.Actions;
+
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        boolean runtime = true;
+
+        Actions fileActions = new Actions();
+
+        while(runtime) {
+            System.out.println("Please select one of the following options: ");
+            System.out.println("1: List the files you are able to play with.");
+            System.out.println("2: List the size of a specific file");
+            
+
+            System.out.println("999: Exit program");
+
+            Scanner scan = new Scanner(System.in);
+            switch(scan.nextInt()) {
+                case 1:
+                    
+                    for (String file1 : fileActions.directoryfiles()) {
+                        System.out.println(file1);    
+                    }
+                    break;
+
+                case 2:
+                    for (String file2 : fileActions.directoryfiles()) {
+                        System.out.println(file2);    
+                    }
+                
+                    System.out.println("Please enter the filename (Hint: file options listed in console)");
+                    System.out.println("Remember to also include the file extension: ");
+
+
+                    Scanner scanning =  new Scanner(System.in);
+                    String file = scanning.nextLine();
+                    System.out.println("File " + file + " has the size of: " + fileActions.fileSize(file) + " bytes.");
+                    break;
+
+                case 3: 
+                    for (String file3 : fileActions.directoryfiles()) {
+                        System.out.println(file3);    
+                    }
+                
+                    System.out.println("Please enter the filename (Hint: file options listed in console)");
+                    System.out.println("Remember to also include the file extension: ");
+
+                    Scanner scanner =  new Scanner(System.in);
+                    String file3 = scanner.nextLine();
+
+                    System.out.println("File contains: " + fileActions.linereader(file3) + " lines.");
+                    
+
+
+
+                case 999: 
+                    runtime = false;
+                    break;
+
+
+            }
+
+            scan.close();
+            System.exit(0);
+        }
+    }
+}
